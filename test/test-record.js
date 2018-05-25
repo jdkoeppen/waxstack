f31edccbfd56b7c67b13245ad7560eb1abc82b68
Justin Koeppengit config --global user.name Justin <jdkoeppen@gmail.com>
2018-05-24 21:18:57 -0700

    Updated PUT test

--- test/test-record.js
+++ test/test-record.js
@@ -1,66 +1,80 @@
 const chai = require("chai");
 const expect = chai.expect;
 const chaiHttp = require("chai-http");
-const { app, runServer, closeServer } = require("../server");
-const { PORT, TEST_DATABASE_URL } = require('./config');
+const {
+  app,
+  runServer,
+  closeServer
+} = require("../server");
+const {
+  PORT,
+  TEST_DATABASE_URL
+} = require('../config');
 
 chai.use(chaiHttp);
 
-describe("record object", function() {
-  before(function() {
+describe("record object", function () {
+  before(function () {
     return runServer(TEST_DATABASE_URL)
 
-  after(function() {
-    return closeServer();
-  });
+    after(function () {
+      return closeServer();
+    });
 
-  it("should POST a record given proper input", function() {
-    const newRecord = {
-      artist: "Jamiroquai",
-      album: "Soundstage Landscape",
-      release: "1998",
-      genre: "Funny Hats",
-      label: "Moving Floor Records"
-    };
-    const expectedKeys = ["_id", "tracks", "__v"].concat(Object.keys(newRecord));
+    it("should POST a record given proper input", function () {
+      const newRecord = {
+        artist: "Jamiroquai",
+        album: "Soundstage Landscape",
+        release: "1998",
+        genre: "Funny Hats",
+        label: "Moving Floor Records"
+      };
+      const expectedKeys = ["_id", "tracks", "__v"].concat(Object.keys(newRecord));
 
-    return chai
-      .request(app)
-      .post("/api/records")
-      .send(newRecord)
-      .then(function(res) {
-        expect(res).to.have.status(201);
-        expect(res).to.be.json;
-        expect(res).to.be.a("object");
-        expect(res.body).to.have.all.keys(expectedKeys);
-        expect(res.body.artist).to.equal(newRecord.artist);
-        expect(res.body.album).to.equal(newRecord.album);
-        expect(res.body.release).to.equal(newRecord.release);
-        expect(res.body.genre).to.equal(newRecord.genre);
-        expect(res.body.label).to.equal(newRecord.label);
-      });
-
-    it("should error if POST does not contain expected keys", function() {
-      const badRequestData = {};
       return chai
         .request(app)
-        .post("/")
-        .send(badRequestData)
-        .catch(function(res) {
-          expect(res).to.have.status(400);
+        .post("/api/records")
+        .send(newRecord)
+        .then(function (res) {
+          expect(res).to.have.status(201);
+          expect(res).to.be.json;
+          expect(res).to.be.a("object");
+          expect(res.body).to.have.all.keys(expectedKeys);
+          expect(res.body.artist).to.equal(newRecord.artist);
+          expect(res.body.album).to.equal(newRecord.album);
+          expect(res.body.release).to.equal(newRecord.release);
+          expect(res.body.genre).to.equal(newRecord.genre);
+          expect(res.body.label).to.equal(newRecord.label);
         });
+
+      it("should error if POST does not contain expected keys", function () {
+        const badRequestData = {};
+        return chai
+          .request(app)
+          .post("/")
+          .send(badRequestData)
+          .catch(function (res) {
+            expect(res).to.have.status(400);
+          });
+      });
     });
+
+    it("should GET a collection of Records", function () {
+      return chai.request(app)
+        .get("/")
+        .then(function (res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res).to.be.a("array")
+          expect(res).to.have.length.of.at.least(1);
+        });
+
+    });
+
+    it("should PUT a user edit to an existing record given a valid ID", function () {
+
+    });
+
+    it("should DELETE a record given a valid ID", function () {});
   });
-
-  it("should GET a collection of Records", function() {
-    return chai.request(app)
-    .get("/")
-    .then(function(res) {
-      expect(res).to.have.status(201)
-    })
-  });
-
-  it("should PUT a user edit to an existing record given a valid ID", function() {});
-
-  it("should DELETE a record given a valid ID", function() {});
-});
+})
